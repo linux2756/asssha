3437be22e139b6f4f3641e40c3f5d8235c5088d7
linux2756 <sarmad2756@gmil.com>
2019-04-23 21:49:49 +0300

    clash free time table

---
+++ aa2/aa.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+word=a
+
+if [[ $word == "b" ]]
+then
+ echo "conditon b is true"
+elif [[ $word == "a" ]]
+then
+ echo "conditon a is true" 
+else
+ echo "conditon is fals"
+fi
---
+++ aa2/demo.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+ "enter your name: " first last
+
+echo "first name:  $first"
+echo "last name:  $last"
